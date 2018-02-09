783ed717d2ab08826013b6fb02c5ce87f743e279
Denis <guoxin.qiu@outlook.com>
2018-02-09 09:29:22 +0800

    add unit test sample

---
+++ tests/Bonbonniere.UnitTests/SampleTest.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Bonbonniere.UnitTests
+{
+    public class SampleTest
+    {
+        [Fact(Skip = "Test")]
+        public void FirstTest()
+        {
+            var a = 1;
+            var b = 1;
+            a.Should().Equals(b);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 4)]
+        public void SecondTest(int firstNumber, int secondNumber)
+        {
+            firstNumber.Should().Be(secondNumber);
+        }
+    }
+}
